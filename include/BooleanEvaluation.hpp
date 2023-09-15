9e57e40873bcfc7aa892b3d68dbb35d3324d5785
Juan Coelho <juancoelho25@hotmail.com>
2023-09-15 16:13:58 -0300

    Organização do plando de código

--- include/BooleanEvaluation.hpp
+++ include/BooleanEvaluation.hpp
@@ -2,8 +2,20 @@
 #define BOOLEANEVALUATION_HPP
 
 #include <string>
+#include <stack>
 
 class BooleanEvaluation{
+
+    //CONSTRUTOR:criar uma função que atribui aos valores de repr_Forumla
+    //os valores em valuation
+
+    //Criar uma função que organiza a string na notação polonesa
+
+    //Criar uma função que realiza a avaliação de expressão usando o 
+    //resultado das funções anteriores
+
+    
+
 
     
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,40 @@
 #include <string>
 #include <stdlib.h>
 #include <errno.h>
-#include "BooleanEvaluation.hpp"
 
 using namespace std;
 
-void parse_args(int argc, char* argv[])
+void parse_args(int argc, char* argv[], string problem_Type)
 // Descricao: Faz o tratamento dos argumentos passados no main()
 {
     //COLOCAR EM UM ARQUIVO SEPARADO PARA MSSGASSERT!
     if(argc != 4){
         cerr << " ./Executable_name " << "<Problem type> " << "<Representation formula> " << "<Valuation>" << endl;
     }
-
-    string problem_Type (argv[1]);
-    string repr_Formula (argv[2]);
-    string valuation    (argv[3]);
-
-    cout << problem_Type << " " << repr_Formula << " " << valuation << endl;
-
     if(problem_Type == "-a"){
-        cout << "avaliacao de expressoes" << endl;
+        cout << "Problema escolhido: Avaliacao de expressoes" << endl;
+        //chamar uma função para receber as strings valuation e repr_formula
     }
     else if(problem_Type == "-s"){
-        cout << "satisfabilidade" << endl;
+        cout << "Problema escolhido: Satisfabilidade" << endl;
     }
 }
 
-int main(int argc, char* argv[]){
     
-    parse_args(argc, argv);
+
+int main(int argc, char* argv[]){
+
+    string problem_Type (argv[1]);
+
+    //String that represents the formula
+    string repr_Formula (argv[2]);
+
+    //String that represents the variable values in the forumla
+    string valuation    (argv[3]);
+
+    parse_args(argc, argv, problem_Type);
+
+    cout << problem_Type << " " << repr_Formula << " " << valuation << endl;
 
     return 0;
 }
