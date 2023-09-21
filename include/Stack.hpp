94345a12073a5dd57aa804e9c6237651bece986f
Juan Coelho <juancoelho25@hotmail.com>
2023-09-21 12:18:43 -0300

    Comentários adicionados em Stack.hpp

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -7,20 +7,34 @@
 template <typename T>
 class Stack {
 private:
-    T* elements;
-    size_t capacity;
-    size_t stackSize;
+    T* elements;        // Pointer to the dynamic array
+    size_t capacity;    // Current capacity of the dynamic array
+    size_t stackSize;   // Number of elements in the stack
 
 public:
+    // Constructor: Initialize the stack with default capacity and size.
     Stack();
+
+    // Push an element onto the stack.
     void push(const T& item);
+
+    // Pop and remove the top element from the stack.
     void pop();
+
+    // Get the top element of the stack.
     T top() const;
+
+    // Check if the stack is empty.
     bool isEmpty() const;
+
+    // Get the number of elements in the stack.
     size_t size() const;
+
+    // Destructor: Free memory allocated for the dynamic array.
     ~Stack();
 };
 
-#endif  // STACK_HPP
+
+#endif
 
 
