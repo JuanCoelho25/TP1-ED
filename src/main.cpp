3ce4367998208ee9c8d193e38181b5c423303413
Juan Coelho <juancoelho25@hotmail.com>
2023-10-04 14:54:28 -0300

    Mudança na função parse_args

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,32 @@ void parse_args(int argc, char* argv[], string problem_Type)
     string valuation    (argv[3]); //String that represents the variable values in the forumla
 
     int size = valuation.size();
-    bool* variableArray = new bool [size];
 
 
     if(problem_Type == "-a"){
         cout << "Problema escolhido: Avaliacao de expressoes" << endl;
 
         // Populate the array with variable-value pairs
+        bool* variableArray = new bool [size];
         for (unsigned int i = 0; i < valuation.size(); ++i) {
             variableArray[i + 1] = (valuation[i] == '1');
         }
 
         BooleanEvaluation avaliacao;
         cout << "Result: " << avaliacao.evaluateExpression(expression, variableArray) << endl;
+        delete variableArray;
     }
 
     else if(problem_Type == "-s"){
         cout << "Problema escolhido: Satisfabilidade" << endl;
+
+        for (char ch : expression) {
+            int i = 0;
+            if(ch == 0 || ch == 1 || ch == 'a' || ch == 'e'){
+                expression[i] = ch;
+            }
+            i++;
+        }
 
         BooleanEvaluation avaliacao;
         cout << "Result: " << avaliacao.satisfiabilityProblem(expression, valuation) << endl;
