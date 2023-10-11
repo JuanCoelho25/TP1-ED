9d64ce342ea7581fea830ca2dd4b233fe51b029a
Juan Coelho <juancoelho25@hotmail.com>
2023-10-11 14:57:20 -0300

    Lançamento de Exceção

--- src/main.cpp
+++ src/main.cpp
@@ -3,47 +3,53 @@
 #include "../include/BinaryTree.hpp"
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
 void parse_args(int argc, char* argv[], string problem_Type)
-// Descricao: Faz o tratamento dos argumentos passados no main()
 {
-
-    string expression   (argv[2]); //String that represents the formula
-    string valuation    (argv[3]); //String that represents the variable values in the forumla
-
-
-    if(problem_Type == "-a"){
-        cout << "Problema escolhido: Avaliacao de expressoes" << endl;
-
-        BooleanEvaluation avaliacao;
-        cout << "Result: " << avaliacao.evaluateExpression(expression, valuation) << endl;
+    if (argc != 4)
+    {
+        cerr << "Usage: ./Executable_name <Problem type> <Representation formula> <Valuation>" << endl;
+        return;
     }
 
-    else if(problem_Type == "-s"){
-        cout << "Problema escolhido: Satisfabilidade" << endl;  
+    string expression (argv[2]);  // String that represents the formula
+    string valuation  (argv[3]);  // String that represents the variable values in the forumla
 
-        BooleanEvaluation avaliacao;
-        avaliacao.satisfiabilityProblem(expression, valuation);
+    try
+    {
+        if (problem_Type == "-a")
+        {
+            cout << "Problema escolhido: Avaliacao de expressoes" << endl;
+
+            BooleanEvaluation avaliacao;
+            cout << "Result: " << avaliacao.evaluateExpression(expression, valuation) << endl;
+        }
+        else if (problem_Type == "-s")
+        {
+            cout << "Problema escolhido: Satisfabilidade" << endl;
+
+            BooleanEvaluation avaliacao;
+            avaliacao.satisfiabilityProblem(expression, valuation);
+        }
+        else
+        {
+            throw invalid_argument("Invalid problem type. Use '-a' for expression evaluation or '-s' for satisfiability.");
+        }
     }
-    //**Tentar fazer um try catch para pegar a exceção quando a opção escolhida é diferente de -a ou -s**
+    catch (const exception &e)
+    {
+        cerr << "Exception caught: " << e.what() << endl;
+    }
 }
 
-    
-
-int main(int argc, char* argv[]){
-
-     //COLOCAR EM UM ARQUIVO SEPARADO PARA MSSGASSERT!
-    if(argc != 4){
-        cerr << " ./Executable_name " << "<Problem type> " << "<Representation formula> " << "<Valuation>" << endl;
-    }
-
+int main(int argc, char *argv[])
+{
     string problem_Type (argv[1]); // What is the problem the user wants
 
     parse_args(argc, argv, problem_Type);
-
-    
 
     return 0;
 }
